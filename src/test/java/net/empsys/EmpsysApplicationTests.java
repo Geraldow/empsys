4beee26af9f058d8351161b53db3e20da7f6c1da
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-07 00:02:13 -0500

    feat(src): add source code structure for the application

---
+++ src/test/java/net/empsys/EmpsysApplicationTests.java
@@ -0,0 +1,13 @@
+package net.empsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
